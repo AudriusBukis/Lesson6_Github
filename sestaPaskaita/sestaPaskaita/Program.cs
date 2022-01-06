aa1be791bf0f26aa6bbb2427ccbab668d6962245
RRokas <rokas.ringaila@gmail.com>
2022-01-06 20:35:50 +0200

    paskutine uzduotis

--- sestaPaskaita/sestaPaskaita/Program.cs
+++ sestaPaskaita/sestaPaskaita/Program.cs
@@ -19,6 +19,16 @@ namespace sestaPaskaita
             earthVolume = 4/3*Math.PI*Math.Pow(r, 3);
             Console.WriteLine($"Earth volume: {earthVolume}");
 
+            Console.WriteLine("**********************");
+            Console.WriteLine("*Viso kintamųjų: 3");
+            Console.WriteLine($"*Kintamasis1: {earthArea}");
+            Console.WriteLine($"*Kintamasis2: {earthVolume}");
+            Console.WriteLine($"*Kintamasis3: {triangleLine}");
+            Console.WriteLine($"*Suma: {earthArea + earthVolume + triangleLine}");
+            Console.WriteLine($"*Vidurkis: {(earthArea + earthVolume + triangleLine)/3}");
+            Console.WriteLine("**********************");
+
+
         }
     }
 }
